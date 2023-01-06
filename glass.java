06f92a9ea60015a601c3e8aeb752905854f2ccae
Your Name <you@example.com>
2023-01-06 15:02:54 -0800

    final day yikes

--- glass.java
+++ glass.java
@@ -8,7 +8,6 @@ public class glass {
     ArrayList<ArrayList<dot>> big = new ArrayList<ArrayList<dot>>();
     ArrayList<color> c= new ArrayList<color>();
     ArrayList<dot> empty = new ArrayList<dot>();
-    //graphics g = new graphics();
     // ArrayList<dot> glass1 = new ArrayList<dot>();
     // ArrayList<dot> glass2 = new ArrayList<dot>();
     // ArrayList<dot> glass3 = new ArrayList<dot>();
@@ -17,47 +16,83 @@ public class glass {
     // ArrayList<dot> glass6 = new ArrayList<dot>();
    
     public glass(){
-       
+       //System.out.println("x");
         int temp = 0;
-        c.add(new color(Color.green,4));
-        c.add(new color(Color.blue,4));
-        c.add(new color(Color.yellow,4));
-        c.add(new color(Color.red,4));
-        c.add(new color(Color.pink,4));
+        int x = 0;
+        while(x!=4){
+            c.add(new color(Color.green));
+            x++;
+        }
+        x=0;
+        while(x!=5){
+            c.add(new color(Color.blue));
+            x++;
+        }
+        x=0;
+        while(x!=5){
+            c.add(new color(Color.yellow));
+            x++;
+        }
+        x=0;
+        while(x!=5){
+            c.add(new color(Color.red));
+            x++;
+        }
+        x=0;
+        while(x!=5){
+            c.add(new color(Color.pink));
+            x++;
+        }
+        x=0;
+
         while (temp < 6){
             big.add(new ArrayList<dot>() );
             temp++;
+            x++;
+            //System.out.println("xx");
         }
-        fillGlasses(5);
+        
         big.add(empty);
+        fillGlasses(5);
+        //System.out.println("x");
        
         
+    }
+    public ArrayList getDotList(){
+        return big;
     }
     
 
     public void fillGlasses(int glassNum){
         int x=0;
         int y=0;
+        //System.out.println(big.size());
         for(ArrayList<dot> l:big){
             int temp = 0;
+           // System.out.println("yy");
             while (temp < 5){
-                l.add(new dot(chooseColor(), x, y));
+               l.add(new dot(chooseColor(), x, y));
                 temp ++;
+                //System.out.println(temp);
             }
         }
     }    
    
     public Color chooseColor(){
+        if (c.size()>0){
+        System.out.println(c.size());
         int rand = r.nextInt(c.size());
-        int num = 0;
-        Color co = Color.black;
-        while (c.get(rand).getNumColor() == 0){
-            rand = r.nextInt(c.size());
-            num=c.get(rand).getNumColor()-1;
-            co=c.get(rand).getColor();
-        }
-        c.set(rand, new color(co,num));
-        return c.get(rand).getColor();
+        return c.remove(rand).getColor();
+    }
+        return Color.black;
+    
+        // System.out.println(c.size());
+        // while (c.get(rand).getNumColor() == 0){
+        //     rand = r.nextInt(c.size());
+       
+        // }
+        // c.get(rand).decreaseByOne();
+        
     }
 
   
